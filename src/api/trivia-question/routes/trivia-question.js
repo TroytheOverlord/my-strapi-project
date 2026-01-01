d78decdd2c74c0199ca307da13cb29e107457075
Colin Peynado Jr <132117593+TroytheOverlord@users.noreply.github.com>
2026-01-01 09:52:13 -0500

    Make trivia-questions endpoint public

--- src/api/trivia-question/routes/trivia-question.js
+++ src/api/trivia-question/routes/trivia-question.js
@@ -1,9 +1,20 @@
-'use strict';
-
-/**
- * trivia-question router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::trivia-question.trivia-question');
+module.exports = {
+  routes: [
+    {
+      method: 'GET',
+      path: '/trivia-questions',
+      handler: 'trivia-question.find',
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: 'GET',
+      path: '/trivia-questions/:id',
+      handler: 'trivia-question.findOne',
+      config: {
+        auth: false,
+      },
+    },
+  ],
+};
